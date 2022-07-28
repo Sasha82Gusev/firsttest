41a13ced5b63d41168516609b236c4d6d82140ca
Sasha82Gusev <105017847+Sasha82Gusev@users.noreply.github.com>
2022-07-29 01:47:22 +0400

    Add files via upload

---
+++ suprrherro.py
@@ -0,0 +1,16 @@
+import requests
+
+max_intelligence = 0
+
+url = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
+response = requests.get(url)
+
+for id in range(len(response.json())):
+    if response.json()[id]['name'] == 'Thanos' or response.json()[id]['name'] == 'Hulk' or response.json()[id]['name'] == 'Captain America':
+        if response.json()[id]['powerstats']['intelligence'] > max_intelligence:
+            max_intelligence_name = response.json()[id]['name']
+
+
+
+
+print(max_intelligence_name)
